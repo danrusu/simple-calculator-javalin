a54af358a091afe0e60afbbf8ae4f442bdf75758
Dan Rusu <dangingko@yahoo.com>
2021-01-26 23:33:42 +0200

    added sl4j dependency

--- src/main/java/app/Server.java
+++ src/main/java/app/Server.java
@@ -3,12 +3,13 @@ package app;
 import io.javalin.Javalin;
 import io.javalin.http.Handler;
 
-public class HelloWorldApp {
+public class Server {
 
     private static Handler apiHandler = ctx -> {
         System.out.println("api path");
         ctx.result("api");
     };
+
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7000);
         app.get("/", ctx -> ctx.result("Hello World"));
