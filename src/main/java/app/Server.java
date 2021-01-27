981272778fa002359180c85020718a1a132f90a2
Dan Rusu <dangingko@yahoo.com>
2021-01-27 11:03:23 +0200

    extracted methods for Javalin routes and configuration setup

--- src/main/java/app/Server.java
+++ src/main/java/app/Server.java
@@ -1,6 +1,7 @@
 package app;
 
 import io.javalin.Javalin;
+import io.javalin.core.JavalinConfig;
 import io.javalin.http.staticfiles.Location;
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,18 +12,29 @@ import static java.lang.Integer.parseInt;
 public class Server {
 
     public static void main(String[] args) {
+        Javalin app = Javalin
+                .create(Server::configurationSetup)
+                .start(getPort());
+
+        setRoutes(app);
+    }
+
+    private static void configurationSetup(JavalinConfig config) {
+        config.addStaticFiles("html/css", Location.EXTERNAL);
+        config.addStaticFiles("html/js", Location.EXTERNAL);
+        config.requestLogger((ctx, ms) -> {
+            log.info(ctx.method() + " " + ctx.fullUrl());
+            log.info(ms + " ms");
+        });
+    }
+
+    private static int getPort() {
         String port = System.getenv("PORT");
         int htmlPort = port != null ? parseInt(port) : 7000;
+        return htmlPort;
+    }
 
-        Javalin app = Javalin.create(config -> {
-                    config.addStaticFiles("html/css", Location.EXTERNAL);
-                    config.addStaticFiles("html/js", Location.EXTERNAL);
-                    config.requestLogger((ctx, ms) -> {
-                        log.info(ctx.method() + " " + ctx.fullUrl());
-                        log.info(ms + " ms");
-                    });
-                }
-        ).start(htmlPort);
+    private static void setRoutes(Javalin app) {
         app.get("/", ctx -> ctx.html(getHtmlIndex()));
         app.get("/api/calculate", SimpleCalculator::calculateHandler);
     }
