3c51f6f341c6826ff1507983c151d08734068ff9
Dan Rusu <dangingko@yahoo.com>
2021-01-27 15:13:59 +0200

    do not accept 'e' js numbers notation - at first

--- src/main/java/app/SimpleCalculator.java
+++ src/main/java/app/SimpleCalculator.java
@@ -7,7 +7,6 @@ import io.javalin.http.Context;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static java.lang.Float.parseFloat;
